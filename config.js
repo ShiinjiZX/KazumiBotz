bec27409d171a6b8838a45d75cb29aac6fedb43c
Shinji De La Kazumi <144513116+ShiinjiZX@users.noreply.github.com>
2025-09-28 16:51:49 +0900

    Upload config.js

---
+++ config.js
@@ -0,0 +1,152 @@
+/*
+Base : Elaina-MultiDevice
+Release : 22 Nov 2022
+*/
+
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import moment from 'moment-timezone'
+
+/*============= WAKTU =============*/
+let wibh = moment.tz('Asia/Jakarta').format('HH')
+let wibm = moment.tz('Asia/Jakarta').format('mm')
+let wibs = moment.tz('Asia/Jakarta').format('ss')
+let wktuwib = `${wibh} H ${wibm} M ${wibs} S`
+let wktugeneral = `${wibh}:${wibm}:${wibs}`
+
+let d = new Date(new Date + 3600000)
+let locale = 'id'
+let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+})
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+/*============= MAIN INFO =============*/
+global.owner = [['6281248845231', 'IkyyKzy', true]]
+global.mods = ["6282381978401","12368709172","6281248845231"]
+global.prems = ["6282381978401","12368709172","6281248845231"]
+global.nomorbot = '6281246493375' // ubah pake no kalian
+global.nomorown = '6281248845231'// ubah pake no kalian
+global.autotyping = true // default mati
+global.autorecording = false // default mati
+
+/*============= WATERMARK =============*/
+global.readMore = readMore
+global.author = 'IkyyKzy'
+global.namebot = 'Kazumi - Botz'
+global.wm = '© Kazumi by IkyyKzy'
+global.watermark = wm
+global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
+global.bottime = `T I M E : ${wktuwib}`
+global.stickpack = `┏━━━━━━━━
+┃Created: IkyyKzy
+┃Number: +${nomorbot}
+┃Sosmed: IG @kzy.zx
+┗━━━\n
+`
+global.stickauth = `\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nᴊᴀɴɢᴀɴ ʟᴜᴘᴀ ʙᴇʀɪʙᴀᴅᴀʜ
+
+Klik ★ dibawah untuk menambah ke favorit.`
+global.week = `${week} ${date}`
+global.wibb = `${wktuwib}`
+
+//*============= SOSMED =============*/
+global.sig = '-'
+global.sgh = '-'
+global.sgc = 'https://chat.whatsapp.com/IYp4pHubQdX5A9ndOWN8Ae'
+global.sgw = '_'
+global.sdc = '-'
+global.sfb = '-'
+global.snh = '-'
+
+// ================= Cpanel ========================================
+global.egg = "15" // gausah di ubah
+
+global.nestid = "5" // gausah diubah
+
+global.loc = "1" // gausah diubah
+
+global.domain = "-" // ini ubah ama domain / web panel lu
+
+global.apikey = "-" // apikey / plta lu
+
+global.capikey = "-" // capikey / pltc lu
+
+/*============= DONASI =============*/
+global.qris = '_'
+global.psaweria = '_'
+
+
+/*============= TAMPILAN =============*/
+global.dmenut = 'ଓ═┅═━–〈' //top
+global.dmenub = '┊↬' //body
+global.dmenub2 = '┊' //body for info cmd on Default menu
+global.dmenuf = '┗––––––––––✦' //footer
+global.dashmenu = '┅═┅═❏ *DASHBOARD* ❏═┅═┅'
+global.cmenut = '❏––––––『' //top
+global.cmenuh = '』––––––' //header
+global.cmenub = '┊✦ ' //body
+global.cmenuf = '┗━═┅═━––––––๑\n' //footer
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     '
+global.pmenus = '✦'
+global.htki = '––––––『' // Hiasan Titile (KIRI)
+global.htka = '』––––––' // Hiasan Title  (KANAN)
+global.lopr = 'Ⓟ' //LOGO PREMIUM ON MENU.JS
+global.lolm = 'Ⓛ' //LOGO LIMIT/FREE ON MENU.JS
+global.htjava = '⫹⫺'    //hiasan Doang :v
+global.hsquere = ['⛶', '❏', '⫹⫺']
+
+global.thumbnail = "https://files.catbox.moe/mm1mtl.jpg"
+global.thumb = "https://files.catbox.moe/l7b77z.jpg"
+global.photo = "https://files.catbox.moe/43keur.jpg"
+
+/*============= RESPON =============*/
+global.wait = 'Please Wait...'
+global.eror = 'Error!'
+
+global.APIs = {
+    ryzen: 'https://api.ryzendesu.vip',
+
+}
+
+/*============= OTHER =============*/
+global.dpptx = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.ddocx = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.dxlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.dpdf = 'application/pdf'
+global.drtf = 'text/rtf'
+
+global.flaaa2 = [
+    "https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=",
+    "https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=",
+    "https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=",
+    "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=",
+    "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text="
+]
+global.fla = [
+    "https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=",
+    "https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=",
+    "https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=",
+    "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=",
+    "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text="
+]
+
+global.thumblvlup = [
+    'https://i.pinimg.com/originals/a0/34/8a/a0348ae908d8ac4ced76df289eb41e1a.jpg',
+    'https://i.pinimg.com/originals/be/3b/47/be3b477371cc249e49fd0bb3284de7d7.jpg',
+    'https://i.pinimg.com/originals/63/c3/37/63c337596b3391df0e72a9729ceca7b6.jpg',
+    'https://i.pinimg.com/originals/db/ed/5a/dbed5afac55d266602d0ca0c67622bb9.jpg'
+]
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+    unwatchFile(file)
+    console.log(chalk.redBright("Update 'config.js'"))
+    import(`${file}?update=${Date.now()}`)
+})
